9dc6511f634884e8513d79d19025db350ba90e5c
Victoria Limwanich <victorialim7721@gmail.com>
2021-11-18 17:51:20 -0800

    commit

--- scripts/chart_2.R
+++ scripts/chart_2.R
@@ -4,3 +4,5 @@ library(patchwork)
 
 parks <- read.csv("C:/Users/toric/Documents/info201/final-project-cjsv/scripts/data/national_parks_biodiversity/parks.csv")
 species <- read.csv("C:/Users/toric/Documents/info201/final-project-cjsv/scripts/data/national_parks_biodiversity/species.csv")
+
+#test
