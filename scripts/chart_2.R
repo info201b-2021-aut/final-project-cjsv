690c6b4127f9350379308599a7abb4ad3f8073ac
Victoria Limwanich <victorialim7721@gmail.com>
2021-11-18 20:31:50 -0800

    commit

--- scripts/chart_2.R
+++ scripts/chart_2.R
@@ -1,9 +1,18 @@
 library(tidyverse)
 library(ggplot2)
 library(patchwork)
+library(maps)
 
 parks <- read.csv("scripts/data/national_parks_biodiversity/parks.csv")
 species <- read.csv("scripts/data/national_parks_biodiversity/species.csv")
 
 states <- merge(x = parks, y = species, all.x = TRUE)
-unique_species <- num(unique(states$order))
+
+unique_species <- states %>%
+  group_by(state)%>%
+  summarize(species_amount = length(unique(order)))
+
+state <- map_data("state")
+chart_2 <- ggplot() +
+  geom_polygon(data = state, aes(x = long, y = lat, group = group), color = "grey", fill = "white") +
+  geom_point(data = biodiversity, aes(x = longitude, y = latitude))
