e3cffeceaa2fde78413fd107033c0078e119b7d9
Victoria Limwanich <victorialim7721@gmail.com>
2021-11-18 17:48:51 -0800

    changes

---
+++ scripts/chart_2.R
@@ -0,0 +1,6 @@
+library(tidyverse)
+library(ggplot2)
+library(patchwork)
+
+parks <- read.csv("C:/Users/toric/Documents/info201/final-project-cjsv/scripts/data/national_parks_biodiversity/parks.csv")
+species <- read.csv("C:/Users/toric/Documents/info201/final-project-cjsv/scripts/data/national_parks_biodiversity/species.csv")
