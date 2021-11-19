a8b7933a3272e4d2d4d598f714fd519887cef522
Victoria Limwanich <victorialim7721@gmail.com>
2021-11-19 14:12:41 -0800

    final changes

--- scripts/chart_2.R
+++ scripts/chart_2.R
@@ -10,6 +10,7 @@ states <- merge(x = parks, y = species, all.x = TRUE)
 unique_species <- states %>%
   group_by(state)%>%
   summarize(species_amount = length(unique(order)))%>%
+  arrange(species_amount)%>%
   mutate(id = seq(1,27))
 
 
@@ -21,11 +22,11 @@ label_data$hjust <- ifelse(angle < -90, 1, 0)
 
 label_data$angle <- ifelse(angle < -90, angle+180, angle)
 
-label_data$state <- paste(labe_data$state, " (", label_data$species_amount, ")", sep="")
+label_data$state <- paste(label_data$state, " (", label_data$species_amount, ")", sep="")
 
-chart_2 <- ggplot(unique_species, aes(x = as.factor(state), y = species_amount))+
+chart_2 <- ggplot(unique_species, aes(x = as.factor(id), y = species_amount))+
   geom_bar(stat="identity", fill=alpha("blue", 0.3))+
-  ylim(-100, 450)+
+  ylim(-100, 530)+
   theme_minimal()+
   theme(
     axis.text = element_blank(),
@@ -34,8 +35,8 @@ chart_2 <- ggplot(unique_species, aes(x = as.factor(state), y = species_amount))
     plot.margin = unit(rep(-2,4), "cm")
   )+
   coord_polar(start = 0) +
-  geom_text(data = label_data, aes(x=state, y=species_amount+10, label = state, hjust = hjust), color = "black", 
+  geom_text(data = label_data, aes(x=id, y=species_amount+10, label = state, hjust = hjust), color = "black", 
             fontface = "bold", alpha = 0.6, size = 2.5, angle = label_data$angle, inherit.aes = FALSE)+
-  geom_text(aes(x=27.5, y=350, label = "Number of Unique Orders in National Parks Across the United States"), 
+  geom_text(aes(x=27.5, y=530, label = "Biodiversity in National Parks Across the States by Number of Unique Orders"), 
             color = "black", inherit.aes = FALSE)
 
