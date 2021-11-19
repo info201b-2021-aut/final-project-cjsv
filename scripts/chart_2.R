b88b7ccfa2e1766b636b9ce7bcd750f7c73bff8c
Victoria Limwanich <victorialim7721@gmail.com>
2021-11-19 13:48:17 -0800

    changes

--- scripts/chart_2.R
+++ scripts/chart_2.R
@@ -12,9 +12,6 @@ unique_species <- states %>%
   summarize(species_amount = length(unique(order)))%>%
   mutate(id = seq(1,27))
 
-unique_species = unique_species %>%
-  arrange(state, species_amount)
-
 
 label_data <- unique_species
 number_of_bar <- nrow(label_data)
@@ -24,6 +21,7 @@ label_data$hjust <- ifelse(angle < -90, 1, 0)
 
 label_data$angle <- ifelse(angle < -90, angle+180, angle)
 
+label_data$state <- paste(labe_data$state, " (", label_data$species_amount, ")", sep="")
 
 chart_2 <- ggplot(unique_species, aes(x = as.factor(state), y = species_amount))+
   geom_bar(stat="identity", fill=alpha("blue", 0.3))+
@@ -37,5 +35,7 @@ chart_2 <- ggplot(unique_species, aes(x = as.factor(state), y = species_amount))
   )+
   coord_polar(start = 0) +
   geom_text(data = label_data, aes(x=state, y=species_amount+10, label = state, hjust = hjust), color = "black", 
-            fontface = "bold", alpha = 0.6, size = 2.5, angle = label_data$angle, inherit.aes = FALSE)
+            fontface = "bold", alpha = 0.6, size = 2.5, angle = label_data$angle, inherit.aes = FALSE)+
+  geom_text(aes(x=27.5, y=350, label = "Number of Unique Orders in National Parks Across the United States"), 
+            color = "black", inherit.aes = FALSE)
 
