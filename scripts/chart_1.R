feff80444e9a3e10d1e9b12784c18101d6c9bc86
jeszhang55 <jzhang55@uw.edu>
2021-11-19 16:48:55 -0800

    chart_1

---
+++ scripts/chart_1.R
@@ -0,0 +1,28 @@
+library(tidyverse)
+library("dplyr")
+library(ggplot2)
+
+parks <- read.csv("data/national_parks_biodiversity/parks.csv", stringsAsFactors = FALSE)
+species <- read.csv("data/national_parks_biodiversity/species.csv", stringsAsFactors = FALSE)
+
+chart_1 <- function(parks, species){
+  speciesandparks <- left_join(species, parks, by = "park_name")
+  
+  num_native_species <- speciesandparks %>%
+  group_by(state) %>%
+  #filter(nativeness == "Native") %>%
+  summarise(native_sum = sum(nativeness == "Native"))
+  
+  num_species_state <- speciesandparks %>%
+  group_by(state) %>%
+  summarise(species_sum = length(nativeness))
+  
+  species_state_table <- merge(num_native_species, num_species_state)
+  
+  species_state_table <- species_state_table %>%
+  mutate(native_species_prop = native_sum / species_sum)
+  
+  return(ggplot(data = species_state_table) +
+  geom_col(mapping = aes(x = state, y = native_species_prop))
+  )
+}
