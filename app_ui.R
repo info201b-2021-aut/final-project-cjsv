706e16d95f5b8e36457a20ae3dc876e585b240bf
Shirleyyao8 <yuhuiyao@uw.edu>
2021-12-08 19:48:53 -0800

    added takeaways

--- app_ui.R
+++ app_ui.R
@@ -203,8 +203,14 @@ takeaways <- tabPanel(
     p("- ewrfwrinfwr"),
     
     #Chart 3
-    p("- ewrfwrinfwr"),
-    p("- ewrfwrinfwr"),
+    p("-The Species Conservation Status scatterplot suggests that Hawaii Volcanoes
+      Park has the highest number of endangered species while Denali National
+      park, Gates of the Arctic National Park, Glacier National Park, Great Sand Dunes
+      , Kobuk Vally National Park has zero endangered species"),
+    p("- Death Vally National Park has the highest number of both threatened
+      species and species of concern"),
+    p("-There are generally under ten endangered species, one to five threatened species,
+      twenty to a hundred species of concern in each national park."),
     
     #Chart 4
     p("- Based on the Conservation in the U.S. map, California has
